56348d18a4f1a6e6c9080e11d6cde1375574ad81
sude <sudefidan@icloud.com>
2024-10-13 18:46:47 +0300

    over-load-metod

---
+++ 20230813_OverLoadMetod/20230813_OverLoadMetod/OverLoadMetodlar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20230813_OverLoadMetod
+{
+    internal class OverLoadMetodlar
+    {
+        public void Topla(int s1,int s2)
+        {
+            Console.WriteLine(s1+s2);
+        }
+        public void Topla(double s1,double s2) 
+        {
+            Console.WriteLine(s1+s2);
+        }
+
+        public void Topla(int s1,int s2,int s3) 
+        {
+            Console.WriteLine(s1+s2+s3);
+        }
+        public void Topla(double s1,double s2,double s3)
+        {
+            Console.WriteLine(s1+s2+s3);
+        }
+    }
+}
---
+++ 20230813_OverLoadMetod/20230813_OverLoadMetod/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20230813_OverLoadMetod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Programımız içerisinde bazı durumlarda aynı fonksiyon adında farklı işleyişler yaptırma imkanı sağlayabiliriz
+            //Örnek olarak içerisine 2 adet int sayıyı parametre olarak tutan bir metodumuz olsun.Bu fonksiyonu çalıştırabilmemiz için fonksiyon çağırma işlemi esnasında 2 adet int sayıyı göndermemiz gerekiyor.Ben aynı fonskiyon adı altın int sayı değilde double sayıları toplamak istediğim durumlarda overload metod yöntemine başvurabilirim
+            OverLoadMetodlar metodlar=new OverLoadMetodlar();
+            Console.Write("1.Fonskiyon = ");
+            metodlar.Topla(5,10);
+
+            Console.Write("2.Fonskiyon = ");
+            metodlar.Topla(5.1,8.3);
+
+            Console.Write("3.Fonskiyon = ");
+            metodlar.Topla(3,8,9);
+
+            Console.Write("4.Fonksiyon = ");
+            metodlar.Topla(2.7,8.4,10.4);
+
+
+            Console.ReadKey();
+        }
+    }
+}
